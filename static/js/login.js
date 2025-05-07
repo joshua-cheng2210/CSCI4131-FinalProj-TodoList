b5c8e468a8230611970be97b91727ea4753423c7
chen7647 <chen7647@umn.edu>
2025-05-07 18:32:08 -0500

    login IP

--- static/js/login.js
+++ static/js/login.js
@@ -1,43 +1,39 @@
-async function onLoginSubmit() {
-    const loginForm = document.getElementById('login-form');
+async function onLoginSubmit(event) {
+    event.preventDefault();
 
-    loginForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
+    const email = document.getElementById('login-email').value.trim();
+    const password = document.getElementById('login-password').value.trim();
 
-        const email = document.getElementById('email').value.trim();
-        const password = document.getElementById('password').value.trim();
+    const loginAcc = {
+        email,
+        password,
+    };
 
-        const loginAcc = {
-            email,
-            password,
-        };
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(loginAcc),
+        });
 
-        try {
-            const response = await fetch('/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(loginAcc),
-            });
-
-            if (response.ok) {
-                const result = await response.json();
-                if (result.success) {
-                    console.log("response: ", response)
-                    window.location.href = '/TodoList.html'; 
-                } else {
-                    window.location.href = '/login.html'; 
-                }
+        if (response.ok) {
+            const result = await response.json();
+            if (result.success) {
+                console.log("response: ", response)
+                window.location.href = '/TodoList.html'; 
             } else {
                 window.location.href = '/login.html'; 
             }
-        } catch (error) {
-            console.error(error);
+        } else {
             window.location.href = '/login.html'; 
         }
-    });
-}
+    } catch (error) {
+        console.error(error);
+        window.location.href = '/login.html'; 
+    }
+};
 
 
 document.addEventListener('DOMContentLoaded', () => {
