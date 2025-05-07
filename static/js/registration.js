227e1a6c626cdc72c4cfefdbec206a140d0b9562
chen7647 <chen7647@umn.edu>
2025-05-07 15:58:43 -0500

    endpoints for registration IP

--- static/js/registration.js
+++ static/js/registration.js
@@ -1,42 +1,39 @@
-async function onRegistrationSubmit() {
-    const registerForm = document.getElementById('register-form');
-
-    registerForm.addEventListener('submit', async (event) => {
-        event.preventDefault(); 
-        
-        const username = document.getElementById('username').value.trim();
-        const email = document.getElementById('email').value.trim();
-        const password = document.getElementById('password').value.trim();
-              
-        const newAcc = {
-            username,
-            email,
-            password,
-        };
-
-        try {
+async function onRegistrationSubmit(event){
+    event.preventDefault(); 
+    
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
             
-            const response = await fetch('/registerAcc', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(newAcc),
-            });
+    const newAcc = {
+        username,
+        email,
+        password,
+    };
 
-            
-            if (response.ok) {
-                const result = await response.json();
-                if (result.success) {
-                    window.location.href = 'login.html'; 
-                }
+    try {
+        const response = await fetch('/registerAcc', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newAcc),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            if (result.success) {
+                window.location.href = 'login.html'; 
             }
-        } catch (error) {
-            console.error(error);
         }
-    });
-}
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-    onRegistrationSubmit()
+    const registerForm = document.getElementById('register-form');
+    if (registerForm) {
+        registerForm.addEventListener('submit', onRegistrationSubmit)
+    }
 });
