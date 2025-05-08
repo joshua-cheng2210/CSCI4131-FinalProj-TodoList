2129d90bc7531e45f910aaccf6bb888896712106
chen7647 <chen7647@umn.edu>
2025-05-08 07:06:40 -0500

    implementing express-session for security

--- server.js
+++ server.js
@@ -256,6 +256,6 @@ app.get('/registration.js', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`server listening on http:`)
+  console.log(`server listening on http://localhost:${port}/`)
 })
   
